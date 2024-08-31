5655fcfa1c63d52c9bbe4a420457c0e0bfc841d7
iqbu <iqbu.lab@gmail.com>
2024-08-31 18:04:07 +0900

    Start Project

---
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by 渡辺大智 on 2024/08/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorial/TwitterSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUITutorialApp.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by 渡辺大智 on 2024/08/31.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
