3c749ccc784f77faadc883f9868e543f102da4dd
iqbu <iqbu.lab@gmail.com>
2024-10-17 09:55:26 +0900

    Registrationロジックの実装

--- TwitterSwiftUITutorial/TwitterSwiftUITutorial/ViewModel/AuthViewModel.swift
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorial/ViewModel/AuthViewModel.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 import Firebase
 import FirebaseAuth
+import FirebaseStorage
+import FirebaseFirestore
 
+// TODO: エラー時のダイアログと処理の切り分け
 class AuthViewModel: ObservableObject {
     func login() {
         
@@ -24,13 +27,45 @@ class AuthViewModel: ObservableObject {
         print("DEBUG: Email is \(email)")
         print("DEBUG: Password is \(password)")
         
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        let filename = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child(filename)
+        
+        // 画像を保存
+        storageRef.putData(imageData, metadata: nil) { _, error in
             if let error = error {
-                print("DEBUG: Error \(error.localizedDescription)")
+                print("DEBUG: Failed to upload image \(error.localizedDescription)")
                 return
             }
             
-            print("DEBUG: Successfully signed up user...")
+            // 画像の保存先取得
+            storageRef.downloadURL { url, _ in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                // emailとpasswordでUser作成
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        print("DEBUG: Error \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    print("DEBUG: Successfully uploaded user photo..")
+                    
+                    // 作成したUser情報
+                    guard let user = result?.user else { return }
+                    let data = [
+                        "email": email,
+                        "username": username,
+                        "fullname": fullName,
+                        "profileImageUrl": profileImageUrl,
+                        "uid": user.uid
+                    ]
+                    
+                    Firestore.firestore().collection("users").document(user.uid).setData(data) { _ in
+                        print("DEBUG: Successfully uploaded user data..")
+                    }
+                }
+            }
         }
     }
 }
